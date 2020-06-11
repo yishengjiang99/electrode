1ad31a671946f84d6977e5f14af95231f0c5cb5a
vn08447 <yisheng.jiang@walmart.com>
2020-06-11 15:40:31 -0700

    save prog

--- packages/subapp-server/lib/jsx-route-renderer.js
+++ packages/subapp-server/lib/jsx-route-renderer.js
@@ -3,7 +3,6 @@ const { updateFullTemplate } = require("./utils");
 const { getSrcDir, setupRouteRender, searchRoutesFromFile } = require("./setup-hapi-routes");
 const Path = require("path");
 const subAppUtil = require("subapp-util");
-
 async function start() {
   const server = await require("@xarc/fastify-server")({
     deferStart: true,
@@ -32,70 +31,165 @@ async function start() {
 
     const chunkSelector = resolveChunkSelector(routeOptions);
     routeOptions.__internals = { chunkSelector };
-    updateSubappPaths(routeOptions);
-    
+    function updateSubappPaths() {
+      if (routeOptions.subApps) {
+        routeOptions.__internals.subApps = [].concat(routeOptions.subApps).map(x => {
+          let options = {};
+          if (Array.isArray(x)) {
+            options = x[1];
+            x = x[0];
+          }
+          // absolute: use as path
+          // else: assume dir under srcDir
+          // TBD: handle it being a module
+          return {
+            subapp: subAppsByPath[Path.isAbsolute(x) ? x : Path.resolve(srcDir, x)],
+            options
+          };
+        });
+      }
+    }
+    updateSubappPaths();
+    const finalTokenHandlers = []
+      .concat([], routeOptions.tokenHandler, routeOptions.tokenHandlers)
+      .filter(x => x);
+
     const templateFile =
       (routeOptions.templateFile && Path.resolve(routeOptions.templateFile)) ||
       Path.join(__dirname, "../template/index");
 
-    const asyncTemplate = initializeTemplate(templateFile, routeOptions);
-    const asyncTemplate = initializeTemplate(defaultSelection, routeOptions);
+    console.log(templateFile);
 
-
-    console.log("routeRenderer.....", routeRenderer);
-  }
-}
-start();
-
-
-const Template = (
-    <IndexPage DOCTYPE="html">
-      <Token _id="INITIALIZE" />
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.11" />
-          <ReserveSpot saveId="headEntries" />
-          <Require _id="subapp-web/lib/polyfill" />
-          <Token _id="META_TAGS" />
-          <Token _id="PAGE_TITLE" />
-          <Require _id="subapp-web/lib/init" />
-  
-          <Token _id="CRITICAL_CSS" />
-        </head>
-        <Token _id="HEAD_CLOSED" />
-        <body>
-          <noscript>
-            <h4>JavaScript is Disabled</h4>
-            <p>Sorry, this webpage requires JavaScript to function correctly.</p>
-            <p>Please enable JavaScript in your browser and reload the page.</p>
-          </noscript>
-          <RenderSubApps />
-          <Require _id="subapp-web/lib/start" />
-        </body>
-        <Token _id="BODY_CLOSED" />
-      </html>
-      <Token _id="HTML_CLOSED" />
-    </IndexPage>
-  );
-function updateSubappPaths() {
-  if (routeOptions.subApps) {
-    routeOptions.__internals.subApps = [].concat(routeOptions.subApps).map(x => {
-      let options = {};
-      if (Array.isArray(x)) {
-        options = x[1];
-        x = x[0];
-      }
-      // absolute: use as path
-      // else: assume dir under srcDir
-      // TBD: handle it being a module
-      return {
-        subapp: subAppsByPath[Path.isAbsolute(x) ? x : Path.resolve(srcDir, x)],
-        options
-      };
+    var jsxRend = new JsxRenderer({
+      templateFullPath: templateFile,
+      template: require(templateFile),
+      tokenHandlers: finalTokenHandlers,
+      insertTokenIds: routeOptions.insertTokenIds,
+      routeOptions
     });
+    jsxRend.render();
   }
 }
+
+const tokenHandlers = {
+  [CONTENT_MARKER]: context => {
+    return (context.user.content && context.user.content.html) || "";
+  },
+
+  [TITLE_MARKER]: () => {
+    return `<title>${routeOptions.pageTitle}</title>`;
+  },
+
+  [APP_CONFIG_DATA_MARKER]: context => {
+    const { webappPrefix } = context.user.routeOptions.uiConfig;
+    const key = `${webappPrefix}${windowConfigKey}`;
+
+    return `<script>window.${key} = window.${key} || {};
+window.${key}.ui = ${JSON.stringify(routeOptions.uiConfig)};
+</script>`;
+  },
+
+  [HEADER_BUNDLE_MARKER]: context => {
+    const manifest = bundleManifest();
+    const manifestLink = manifest ? `<link rel="manifest" href="${manifest}" />\n` : "";
+    const css = [].concat(WEBPACK_DEV ? context.user.devCSSBundle : context.user.cssChunk);
+
+    const cssLink = css.reduce((acc, file) => {
+      file = WEBPACK_DEV ? file : prodBundleBase + file.name;
+      return `${acc}<link rel="stylesheet" href="${file}" />`;
+    }, "");
+
+    const htmlScripts = htmlifyScripts(
+      groupScripts(routeOptions.unbundledJS.enterHead).scripts,
+      context.user.scriptNonce
+    );
+
+    return `${manifestLink}${cssLink}${htmlScripts}`;
+  },
+
+  [BODY_BUNDLE_MARKER]: context => {
+    context.user.query = context.user.request.query;
+    const js = bundleJs(context.user);
+    const jsLink = js ? { src: js } : "";
+
+    const ins = routeOptions.unbundledJS.preBundle.concat(
+      jsLink,
+      routeOptions.unbundledJS.postBundle
+    );
+    const htmlScripts = htmlifyScripts(groupScripts(ins).scripts, context.user.scriptNonce);
+
+    return `${htmlScripts}`;
+  },
+
+  [DLL_BUNDLE_MARKER]: context => {
+    if (WEBPACK_DEV) {
+      return makeElectrodeDllScripts(loadElectrodeDllAssets(context.user.routeOptions));
+    }
+
+    if (context.user.routeData.dllAssetScripts === undefined) {
+      context.user.routeData.dllAssetScripts = makeElectrodeDllScripts(
+        loadElectrodeDllAssets(context.user.routeOptions),
+        context.user.scriptNonce
+      );
+    }
+
+    return context.user.routeData.dllAssetScripts;
+  },
+
+  [PREFETCH_MARKER]: prefetchBundles,
+
+  [META_TAGS_MARKER]: iconStats,
+
+  [CRITICAL_CSS_MARKER]: context => {
+    return criticalCSS ? `<style${context.user.styleNonce}>${criticalCSS}</style>` : "";
+  },
+
+  [WEBAPP_START_SCRIPT_MARKER]: context => {
+    const { webappPrefix } = context.user.routeOptions.uiConfig;
+    /* istanbul ignore next */
+    const startFuncName = webappPrefix ? `${webappPrefix}WebappStart` : "webappStart";
+    return `<script>
+if (window["${startFuncName}"]) window["${startFuncName}"]();
+</script>`;
+  },
+
+  INITIALIZE,
+  HEAD_INITIALIZE: null,
+  HEAD_CLOSED: null,
+  AFTER_SSR_CONTENT: null,
+  BODY_CLOSED: null,
+  HTML_CLOSED: null
+};
+// const Template = (
+//     <IndexPage DOCTYPE="html">
+//       <Token _id="INITIALIZE" />
+//       <html lang="en">
+//         <head>
+//           <meta charset="UTF-8" />
+//           <meta name="viewport" content="width=device-width, initial-scale=1.11" />
+//           <ReserveSpot saveId="headEntries" />
+//           <Require _id="subapp-web/lib/polyfill" />
+//           <Token _id="META_TAGS" />
+//           <Token _id="PAGE_TITLE" />
+//           <Require _id="subapp-web/lib/init" />
+
+//           <Token _id="CRITICAL_CSS" />
+//         </head>
+//         <Token _id="HEAD_CLOSED" />
+//         <body>
+//           <noscript>
+//             <h4>JavaScript is Disabled</h4>
+//             <p>Sorry, this webpage requires JavaScript to function correctly.</p>
+//             <p>Please enable JavaScript in your browser and reload the page.</p>
+//           </noscript>
+//           <RenderSubApps />
+//           <Require _id="subapp-web/lib/start" />
+//         </body>
+//         <Token _id="BODY_CLOSED" />
+//       </html>
+//       <Token _id="HTML_CLOSED" />
+//     </IndexPage>
+//   );
 
 function resolveChunkSelector(options) {
   if (options.bundleChunkSelector) {
@@ -107,54 +201,54 @@ function resolveChunkSelector(options) {
     js: "main"
   });
 }
+start();
 
-function makeRouteHandler(routeOptions) {
-  routeOptions._templateCache = {};
-  let defaultSelection;
+// function makeRouteHandler(routeOptions) {
+//   routeOptions._templateCache = {};
+//   let defaultSelection;
 
-  if (routeOptions.templateFile) {
-    defaultSelection = {
-      templateFile:
-        typeof routeOptions.templateFile === "string"
-          ? Path.resolve(routeOptions.templateFile)
-          : Path.join(__dirname, "../template/index")
-    };
-  } else {
-    defaultSelection = { htmlFile: routeOptions.htmlFile };
-  }
+//   if (routeOptions.templateFile) {
+//     defaultSelection = {
+//       templateFile:
+//         typeof routeOptions.templateFile === "string"
+//           ? Path.resolve(routeOptions.templateFile)
+//           : Path.join(__dirname, "../template/index")
+//     };
+//   } else {
+//     defaultSelection = { htmlFile: routeOptions.htmlFile };
+//   }
 
-  const render = (options, templateSelection) => {
-    let selection = templateSelection || defaultSelection;
-    if (templateSelection && !templateSelection.templateFile && !templateSelection.htmlFile) {
-      selection = Object.assign({}, templateSelection, defaultSelection);
-    }
-    const asyncTemplate = initializeTemplate(selection, routeOptions);
-    return asyncTemplate.render(options);
-  };
+//   const render = (options, templateSelection) => {
+//     let selection = templateSelection || defaultSelection;
+//     if (templateSelection && !templateSelection.templateFile && !templateSelection.htmlFile) {
+//       selection = Object.assign({}, templateSelection, defaultSelection);
+//     }
+//     const asyncTemplate = initializeTemplate(selection, routeOptions);
+//     return asyncTemplate.render(options);
+//   };
 
-  return options => {
-    // if (routeOptions.selectTemplate) {
-    //   const selection = routeOptions.selectTemplate(options.request, routeOptions);
+//   return options => {
+//     // if (routeOptions.selectTemplate) {
+//     //   const selection = routeOptions.selectTemplate(options.request, routeOptions);
 
-    //   if (selection && selection.then) {
-    //     return selection.then(x => render(options, x));
-    //   }
+//     //   if (selection && selection.then) {
+//     //     return selection.then(x => render(options, x));
+//     //   }
 
-    //   return render(options, selection);
-    // }
-    const templateFile =
-      (routeOptions.templateFile && Path.resolve(routeOptions.templateFile)) ||
-      Path.join(__dirname, "../template/index");
+//     //   return render(options, selection);
+//     // }
+//     const templateFile =
+//       (routeOptions.templateFile && Path.resolve(routeOptions.templateFile)) ||
+//       Path.join(__dirname, "../template/index");
 
+//       function initializeTemplate(
+//         { htmlFile, templateFile, tokenHandlers, cacheId, cacheKey, options },
+//         routeOptions
+//       ) {
 
-      function initializeTemplate(
-        { htmlFile, templateFile, tokenHandlers, cacheId, cacheKey, options },
-        routeOptions
-      ) {
-
-      return asyncTemplate.render(options);
-  };
-}
+//       return asyncTemplate.render(options);
+//   };
+// }
 
 // function routeRender(srcDir, topOpts) {
 //   updateFullTemplate(srcDir, routeOptions);
