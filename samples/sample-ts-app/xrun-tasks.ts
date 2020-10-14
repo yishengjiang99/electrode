c8a31ca2ebffb97b26a978988c4e534af5cbfed5
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-10-14 16:42:14 -0700

    sample ts app

---
+++ samples/sample-ts-app/xrun-tasks.ts
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var xarcOptions = {
+  babelOption: {
+    enableTypeScript: true,
+  }
+};
+
+var xrun = require('@xarc/run');
+
+require("@xarc/app")(xrun, xarcOptions);
